85cdbf25e7d9b3571e35063f1ac8060589df86c9
Wendyz-811 <yz11326@nyu.edu>
2025-04-02 16:01:07 -0400

    new

--- api.py
+++ api.py
@@ -3,18 +3,18 @@ import base64
 import requests
 from flask import Flask, request, jsonify, render_template
 
-app = Flask(__name__)  # 初始化 Flask 应用
+app = Flask(__name__)  
 
 # i put my hugging face model here. And I copied my access token.
 API_URL = "https://router.huggingface.co/hf-inference/models/black-forest-labs/FLUX.1-dev"
 HF_TOKEN = os.getenv("HF_TOKEN")
 
-# 主页路由，渲染 index.html（放在 templates 文件夹里）
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Meme 生成 API 接口，前端用 fetch POST 到这里
+
 @app.route('/api/generate', methods=['POST'])
 def generate():
     data = request.get_json()
